c875d65a82f8ab20291a5a222ae05b57360221fd
urisabate <oriolsabate97@hotmail.com>
2018-09-17 21:27:39 +0200

    na de na

--- vec3class/vec3class/main.cpp
+++ vec3class/vec3class/main.cpp
@@ -1,13 +1,7 @@
 #include "vec3.h"
 
 
-int main() {
-
-	vec3<int> v1(1, 2, 3);
-	vec3<int> v2(v1);
-	vec3<int> v3;
-	
-	cout << v1.operator+=(3) << endl;
+int main(){	
 
 	system("pause");
 	return 0;
