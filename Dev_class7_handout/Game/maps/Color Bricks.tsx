47d2b1a6e679af03f0d3bdeea5c9fcc1a4cf9a6f
urisabate <oriolsabate97@hotmail.com>
2018-10-10 01:21:43 +0200

    maplvl2 loaded but not running well

---
+++ Dev_class7_handout/Game/maps/Color Bricks.tsx
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.1" tiledversion="2018.09.12" name="ColorBricks" tilewidth="32" tileheight="32" tilecount="392" columns="28">
+ <image source="Tilesheet/platformPack_tilesheet.png" width="896" height="448"/>
+ <terraintypes>
+  <terrain name="Yellow" tile="14"/>
+  <terrain name="Red" tile="70"/>
+  <terrain name="Blue" tile="12"/>
+  <terrain name="Green" tile="68"/>
+  <terrain name="Yellow2" tile="120"/>
+  <terrain name="Brown" tile="176"/>
+  <terrain name="Silver" tile="232"/>
+  <terrain name="BrownBrick" tile="174"/>
+  <terrain name="SilverBrick" tile="230"/>
+  <terrain name="Box" tile="288"/>
+  <terrain name="New Terrain" tile="286"/>
+ </terraintypes>
+ <tile id="12" terrain="2,2,2,2"/>
+ <tile id="13" terrain="2,2,2,2"/>
+ <tile id="14" terrain="0,0,0,0"/>
+ <tile id="15" terrain="0,0,0,0"/>
+ <tile id="40" terrain="2,2,2,2"/>
+ <tile id="41" terrain="2,2,2,2"/>
+ <tile id="42" terrain="0,0,0,0"/>
+ <tile id="43" terrain="0,0,0,0"/>
+ <tile id="68" terrain="3,3,3,3"/>
+ <tile id="69" terrain="3,3,3,3"/>
+ <tile id="70" terrain="1,1,1,1"/>
+ <tile id="71" terrain="1,1,1,1"/>
+ <tile id="96" terrain="3,3,3,3"/>
+ <tile id="97" terrain="3,3,3,3"/>
+ <tile id="98" terrain="1,1,1,1"/>
+ <tile id="99" terrain="1,1,1,1"/>
+ <tile id="120" terrain="4,4,4,4"/>
+ <tile id="121" terrain="4,4,4,4"/>
+ <tile id="148" terrain="4,4,4,4"/>
+ <tile id="149" terrain="4,4,4,4"/>
+ <tile id="174" terrain="7,7,7,7"/>
+ <tile id="175" terrain="7,7,7,7"/>
+ <tile id="176" terrain="5,5,5,5"/>
+ <tile id="177" terrain="5,5,5,5"/>
+ <tile id="202" terrain="7,7,7,7"/>
+ <tile id="203" terrain="7,7,7,7"/>
+ <tile id="204" terrain="5,5,5,5"/>
+ <tile id="205" terrain="5,5,5,5"/>
+ <tile id="230" terrain="8,8,8,8"/>
+ <tile id="231" terrain="8,8,8,8"/>
+ <tile id="232" terrain="6,6,6,6"/>
+ <tile id="233" terrain="6,6,6,6"/>
+ <tile id="258" terrain="8,8,8,8"/>
+ <tile id="259" terrain="8,8,8,8"/>
+ <tile id="260" terrain="6,6,6,6"/>
+ <tile id="261" terrain="6,6,6,6"/>
+ <tile id="286" terrain="10,10,10,10"/>
+ <tile id="287" terrain="10,10,10,10"/>
+ <tile id="288" terrain="9,9,9,9"/>
+ <tile id="289" terrain="9,9,9,9"/>
+ <tile id="314" terrain="10,10,10,10"/>
+ <tile id="315" terrain="10,10,10,10"/>
+ <tile id="316" terrain="9,9,9,9"/>
+ <tile id="317" terrain="9,9,9,9"/>
+</tileset>
--- Dev_class7_handout/Motor2D/j1Map.cpp
+++ Dev_class7_handout/Motor2D/j1Map.cpp
@@ -176,7 +176,7 @@ bool j1Map::Load(const char* file_name)
 	bool ret = true;
 	p2SString tmp("maps\\%s", folder.GetString(), file_name);
 
-	pugi::xml_parse_result result = map_file.load_file("maps/iso_walk.tmx");
+	pugi::xml_parse_result result = map_file.load_file("maps/MapLvl2.tmx");
 
 	if(result == NULL)
 	{
