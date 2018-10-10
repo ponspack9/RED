762d8ae5423867102f4c0cd7c3f8f78b8e1724e6
urisabate <36154193+urisabate@users.noreply.github.com>
2018-10-10 12:41:15 +0200

    mapLvl2 colliders added(TILED)

--- Dev_class7_handout/Game/maps/Color Bricks.tsx
+++ Dev_class7_handout/Game/maps/Color Bricks.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.1" tiledversion="2018.09.12" name="ColorBricks" tilewidth="32" tileheight="32" tilecount="392" columns="28">
+<tileset name="ColorBricks" tilewidth="32" tileheight="32" tilecount="392" columns="28">
  <image source="Tilesheet/platformPack_tilesheet.png" width="896" height="448"/>
  <terraintypes>
   <terrain name="Yellow" tile="14"/>
